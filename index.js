7d2b51e11f36831ae6caec9844f099b64e7f0f23
Lucajas <91571540+Lucajas@users.noreply.github.com>
2025-11-25 13:31:26 -0300

    Update index.js
    Removed commented-out code and added import statement.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// index.js
-
-import 'react-native-gesture-handler'; // <-- ADICIONE ISSO AQUI
+import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-// registerRootComponent chama AppRegistry.registerComponent('main', () => App);
-// ...
 registerRootComponent(App);
